18a9574ebc508cbc22162ca932006223e1ff5cd4
darth.revan.030 <smartsamarth03@gmail.com>
2026-04-06 17:02:57 +0800

    feat: add Vercel Analytics and Speed Insights to enhance performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AppProvider } from './store/appContext';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from "@vercel/analytics"
 import App from './App';
 import './styles/tokens.css';
 import './styles/base.css';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('No #root element found');
+
+// Vercel Analytics
+inject();
+injectSpeedInsights();
 
 createRoot(root).render(
   <StrictMode>
